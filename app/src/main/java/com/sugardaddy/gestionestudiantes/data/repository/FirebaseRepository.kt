082415a0892a8e7489fd3b01ae2cdbd684bc6ebc
PJ211152 <perezxavier918@gmail.com>
2025-04-18 18:21:25 -0600

    feat: Agregado el mostrar registros y eliminarlos

--- app/src/main/java/com/sugardaddy/gestionestudiantes/data/repository/FirebaseRepository.kt
+++ app/src/main/java/com/sugardaddy/gestionestudiantes/data/repository/FirebaseRepository.kt
@@ -1,6 +1,9 @@
 package com.sugardaddy.gestionestudiantes.data.repository
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.sugardaddy.gestionestudiantes.data.model.Estudiante
 
 object FirebaseRepository {
@@ -11,5 +14,26 @@ object FirebaseRepository {
         dbRef.child(id).setValue(estudiante)
             .addOnSuccessListener { onSuccess() }
             .addOnFailureListener { onError(it.message ?: "Error desconocido") }
+    }
+
+    fun llamarRegistros(onSuccess: (List<Estudiante>) -> Unit, onError: (DatabaseError) -> Unit){
+        dbRef.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot){
+                val lista = mutableListOf<Estudiante>()
+                for(objeto in snapshot.children){
+                    val estudiante = objeto.getValue(Estudiante::class.java)
+                    estudiante?.let {lista.add(it)}
+                }
+                onSuccess(lista)
+            }
+            override fun onCancelled(error: DatabaseError){onError(error)}
+        })
+    }
+
+    fun eliminarEstudiante(carnet: String, onSuccess: () -> Unit, onError: (DatabaseError) -> Unit) {
+        val ref = dbRef.child(carnet)
+        ref.removeValue()
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onError(DatabaseError.fromException(it)) }
     }
 }
--- app/src/main/java/com/sugardaddy/gestionestudiantes/navigation/NavGraph.kt
+++ app/src/main/java/com/sugardaddy/gestionestudiantes/navigation/NavGraph.kt
@@ -6,11 +6,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.sugardaddy.gestionestudiantes.ui.principal.PantallaPrincipal
 import com.sugardaddy.gestionestudiantes.ui.registrar.PantallaRegistro
+import com.sugardaddy.gestionestudiantes.ui.mostrar.ListaEstudiantes
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController, startDestination = "registro") {
+    NavHost(navController, startDestination = "mostrar") {
         composable("registro") { PantallaRegistro(navController) }
         composable("principal") { PantallaPrincipal(navController) }
+        composable("mostrar") { ListaEstudiantes(navController) }
     }
 }
